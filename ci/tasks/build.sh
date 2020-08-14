a7188dace77205291be1dc21d798f652695525f9
Gopal <gopalakrishnan_selvaraj@comcast.com>
2020-08-14 11:24:32 +0530

    build.sh

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -10,8 +10,8 @@ version="$(mvn help:evaluate -Drevision="${revision}" -Dexpression=project.versi
 mvn clean package \
   -Drevision="${revision}"
 
-cp target/hello-world-*.war ../build
-cp pom.xml ../build
-mkdir ../build/codecoverage
-cp target/site/jacoco-ut/index.html ../build/codecoverage/index.html
+#cp target/hello-world-*.war ../build
+#cp pom.xml ../build
+#mkdir ../build/codecoverage
+#cp target/site/jacoco-ut/index.html ../build/codecoverage/index.html
 echo "${version}" > ../build/version
