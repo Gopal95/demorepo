b5bab1e8c506f7b21f56d18e593fc5892a27ba2c
Gopal <gopalakrishnan_selvaraj@comcast.com>
2020-08-14 11:14:20 +0530

    build.sh

---
+++ ci/tasks/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+cd appcode
+
+revision="$(date '+%Y%m%d%H%M%S')+git.sha.$(cat .git/short_ref)"
+version="$(mvn help:evaluate -Drevision="${revision}" -Dexpression=project.version -q -DforceStdout)"
+
+mvn clean package \
+  -Drevision="${revision}"
+
+cp target/hello-world-*.war ../build
+cp pom.xml ../build
+mkdir ../build/codecoverage
+cp target/site/jacoco-ut/index.html ../build/codecoverage/index.html
+echo "${version}" > ../build/version
